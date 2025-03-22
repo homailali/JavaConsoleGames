e8a0b7318d70cb5892e1ce8012223930338d4fb3
homailali <homailali56@gmail.com>
2025-03-22 14:03:32 +0500

    Update

--- Hangman/HangManClass.java
+++ Hangman/HangManClass.java
@@ -2,7 +2,7 @@ package com.Homail.Games.Hangman;
 import java.util.Scanner;
 import java.util.Random;
 public class HangManClass {
-    // changing something
+    // changing something if it isn't up to date
     Scanner read=new Scanner(System.in);
     Random random=new Random();
     int trackOfHangManBuild=0;
